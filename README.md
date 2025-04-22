24ed137d164b40766bd7853d387bd76c041ff37d
Julio-Garnica <juliogarnica@ciencias.unam.mx>
2025-04-21 19:16:56 -0600

    Práctica 4. Poniendo orden en las carpetas del recpositorio.

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
 # Practicas-Laboratorio-ICC
-PASOS PARA EJECUTAR EL PROGRAMA. 
-1. Ubicarse en su terminal con la ruta en donde se encuentra este archivo.
-2. Compilar el código con: javac (NombredelArchivo).java
-3. Ejecutar el código con: java (NombredelArchivo)
-4. Seguir las indicaciones propias del programa para obtener los resultados esperados.
-
 
 Julio César Lozano Garnica 420095390
---
+++ practicas/practica01/Main.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void imprimirCalculadora(){
+        System.out.println("---------------");
+        System.out.println("- CALCULADORA -");
+        System.out.println("---------------");
+    }
+
+    public static void imprimirOpciones(){
+        System.out.println("La opcion 1 suma dos numeros dados por el usuario.");
+        System.out.println("La opcion 2 multiplica dos numeros dados por el usuario.");
+        System.out.println("La opcion 3 divide dos numeros dados por el usuario.");
+        System.out.println("La opcion 4 repite una oracion dada por el usuario");
+    }
+    
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        imprimirCalculadora();
+        imprimirOpciones();
+
+        System.out.println("Proporciona el numero de la opcion que te gustaria realizar.");
+        int x = scanner.nextInt();
+        scanner.nextLine();
+
+        if (x==1) {
+            System.out.println("Ingresa el primer numero");
+            double y = scanner.nextDouble();
+            System.out.println("Ingresa el segundo numero");
+            double z = scanner.nextDouble();
+            Double suma = y + z;
+            System.out.println("La suma del primero numero mas el segundo es igual a: " + suma);
+        }else if (x==2) {
+            System.out.println("Ingresa el primer numero");
+            double y = scanner.nextDouble();
+            System.out.println("Ingresa el segundo numero");
+            double z = scanner.nextDouble();
+            Double producto = y * z;
+            System.out.println("El producto del primer numero por el segundo es igual a: " + producto);
+        }else if (x==3) {
+            System.out.println("Ingresa el primer numero");
+            double y = scanner.nextDouble();
+            System.out.println("Ingresa el segundo numero");
+            double z = scanner.nextDouble();
+            Double division = y / z;
+            System.out.println("La division del primer numero entre el segundo es igual a: " + division);
+        }else if (x==4) {
+            System.out.println("Teclea una oracion");
+            String oracion = scanner.nextLine();
+            System.out.println("La oracion dada por el usuario es: " + oracion);
+        }else {
+            System.out.println("No existe esa opcion. Ingresa un numero entre 1, 2 o 3.");
+        }
+        scanner.close();
+    }
+}
